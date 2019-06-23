c93974e9d836fe6335ed3fd68e937705478595ac
ALMLAMBO <aleksm007148@gmail.com>
2019-06-23 12:12:05 +0300

    Add country model

---
+++ BookApp/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApp.Models {
+	public class Country {
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
