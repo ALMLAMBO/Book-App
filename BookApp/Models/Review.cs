8ed6787da9031ef71d50424ea33bcb1e0891dbdd
ALMLAMBO <aleksm007148@gmail.com>
2019-06-23 12:41:48 +0300

    Add all annotations and error messages

--- BookApp/Models/Review.cs
+++ BookApp/Models/Review.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookApp.Models {
 	public class Review {
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int Id { get; set; }
+		[Required]
+		[StringLength(200, MinimumLength = 10, ErrorMessage = "Headline must be between 10 and 200 characters")]
 		public string HeadLine { get; set; }
+		[Required]
+		[StringLength(2000, MinimumLength = 50, ErrorMessage = "Review text must be between 50 and 2000 characters")]
 		public string ReviewText { get; set; }
 		public int Rating { get; set; }
 		public virtual Reviewer Reviewer { get; set; }
