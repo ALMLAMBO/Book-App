2bf7f9e6e1b36bc8a0e852a1a3e7b0ad8489b4bf
ALMLAMBO <aleksm007148@gmail.com>
2019-06-23 12:21:51 +0300

    Add collection for the authors

--- BookApp/Models/Country.cs
+++ BookApp/Models/Country.cs
@@ -7,5 +7,6 @@ namespace BookApp.Models {
 	public class Country {
 		public int Id { get; set; }
 		public string Name { get; set; }
+		public virtual ICollection<Author> Authors { get; set; }
 	}
 }
