d3fa20dcaf196949102ff64649034a44cd90e7df
ALMLAMBO <aleksm007148@gmail.com>
2019-06-23 12:12:31 +0300

    Add reviewer model

---
+++ BookApp/Models/Reviewer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApp.Models {
+	public class Reviewer {
+		public int Id { get; set; }
+		public string FisrtName { get; set; }
+		public string LastName { get; set; }
+	}
+}
