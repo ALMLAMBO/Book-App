aa24e67b17b13ee0d6e6b1788dd247b7b3a70c98
ALMLAMBO <aleksm007148@gmail.com>
2019-06-23 11:54:14 +0300

    Add configuration for using mvc

--- BookApp/Startup.cs
+++ BookApp/Startup.cs
@@ -12,6 +12,7 @@ namespace BookApp {
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services) {
+			services.AddMvc();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,6 +24,8 @@ namespace BookApp {
 			app.Run(async (context) => {
 				await context.Response.WriteAsync("Hello World!");
 			});
+
+			app.UseMvc();
 		}
 	}
 }
