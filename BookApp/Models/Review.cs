fb3e1b03daa739f4180cb7c5ede7e9bf104ed453
ALMLAMBO <aleksm007148@gmail.com>
2019-06-23 12:44:53 +0300

    Add range for the rating with error message

--- BookApp/Models/Review.cs
+++ BookApp/Models/Review.cs
@@ -17,6 +17,7 @@ namespace BookApp.Models {
 		[StringLength(2000, MinimumLength = 50, ErrorMessage = "Review text must be between 50 and 2000 characters")]
 		public string ReviewText { get; set; }
 		[Required]
+		[Range(1, 5, ErrorMessage = "Rating must be between 1 and 5 stars")]
 		public int Rating { get; set; }
 		public virtual Reviewer Reviewer { get; set; }
 		public virtual Book Book { get; set; }
