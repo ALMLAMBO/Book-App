8575278f750bcf3ba168dcb7a99f211a13bf1363
ALMLAMBO <aleksm007148@gmail.com>
2019-06-23 14:02:55 +0300

    Comment seeding data

--- BookApp/Startup.cs
+++ BookApp/Startup.cs
@@ -31,14 +31,16 @@ namespace BookApp {
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, BookDbContext context) {
 			if (env.IsDevelopment()) {
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.Run(async (context) => {
-				await context.Response.WriteAsync("Hello World!");
-			});
+			//app.Run(async (context) => {
+			//	await context.Response.WriteAsync("Hello World!");
+			//});
+
+			//context.SeedDataContext();
 
 			app.UseMvc();
 		}
