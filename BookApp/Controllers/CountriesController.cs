da11b4de4adfe0efe17d29076dff0b6c4fe595fa
ALMLAMBO <aleksm007148@gmail.com>
2019-06-23 15:43:04 +0300

    Add countries controller

---
+++ BookApp/Controllers/CountriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApp.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CountriesController : Controller {
+		private ICountryRepository _countryRepository;
+
+		public CountriesController(ICountryRepository countryRepository) {
+			_countryRepository = countryRepository;
+		}
+
+		//api/countries
+		[HttpGet]
+		public ActionResult GetCountries() {
+			return Ok(_countryRepository
+				.GetCountries().ToList()
+			);
+		}
+	}
+}
