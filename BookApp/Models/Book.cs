6ebf7dcf16186091d17c93f39e75f3c26bd022be
ALMLAMBO <aleksm007148@gmail.com>
2019-06-23 12:11:45 +0300

    Add book model

---
+++ BookApp/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApp.Models {
+	public class Book {
+		public int Id { get; set; }
+		public string ISBN { get; set; }
+		public string Title { get; set; }
+		public DateTime DatePublished { get; set; }
+	}
+}
