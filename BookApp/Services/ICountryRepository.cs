3ba8819bab2f7160532f1164506323a68a1e2a6b
ALMLAMBO <aleksm007148@gmail.com>
2019-06-23 14:08:37 +0300

    Add country interface

---
+++ BookApp/Services/ICountryRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApp.Services {
+	public interface ICountryRepository {
+		ICollection<Country> GetCountries();
+		Country GetCountry(int countryId);
+		Country GetCountryOfAnAuthor(int authorId);
+		ICollection<Author> GetAuthorsFromACountry(int countryId);
+	}
+}
