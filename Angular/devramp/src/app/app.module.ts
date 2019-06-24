043ec6731246937b2c72cfad4848cd01233935ec
ALMLAMBO <aleksm007148@gmail.com>
2019-06-25 00:07:35 +0300

    Add service for the author and grid component for the author

--- Angular/devramp/src/app/app.module.ts
+++ Angular/devramp/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthorService } from './author.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AuthorComponent } from './author/author.component';
+import { GridAuhtorsComponent } from './grid-auhtors/grid-auhtors.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent }
@@ -15,14 +18,18 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
-    AuthorComponent
+    AuthorComponent,
+    GridAuhtorsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthorService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
