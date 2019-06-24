068e39d3aa75406420e5a1f1603233f2b2d0429c
ALMLAMBO <aleksm007148@gmail.com>
2019-06-24 23:45:40 +0300

    Add component which will display authors in grid system

---
+++ Angular/devramp/src/app/grid-auhtors/grid-auhtors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-auhtors',
+  templateUrl: './grid-auhtors.component.html',
+  styleUrls: ['./grid-auhtors.component.css']
+})
+export class GridAuhtorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
