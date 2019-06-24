4b553fd0692d63b43ae3ffe51de75e01f822349c
ALMLAMBO <aleksm007148@gmail.com>
2019-06-25 00:20:12 +0300

    Add add-or-update component for author

---
+++ Angular/devramp/src/app/add-or-update-author/add-or-update-author.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-or-update-author',
+  templateUrl: './add-or-update-author.component.html',
+  styleUrls: ['./add-or-update-author.component.css']
+})
+export class AddOrUpdateAuthorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
